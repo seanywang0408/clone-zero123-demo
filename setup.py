823b58348a095c192e5bf3cc661a99aca316012d
seanywang0408 <1043653815@qq.com>
2023-04-27 18:08:49 +0800

    debug

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+import os
+
+os.system('wget -cP /home/xlab-app-center/ https://cv.cs.columbia.edu/zero123/assets/105000.ckpt')
+print('ckpt exist?', os.path.isfile('./105000.ckpt'))
 
 setup(
     name='latent-diffusion',
